f14c84bf3147d637de8731e0c89221eee98b3282
Robert Hutchins <rjhutchins22@gmail.com>
2017-06-14 13:54:38 -0400

    Working on Js pushing for time

--- customers/main.js
+++ customers/main.js
@@ -1,10 +1,69 @@
 // 1. Fetch your users data
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
+console.log('you connected dawg');
+
 (function () {
 
   'use strict';
+  fetch('https://randomuser.me/api/?results=12')
+    .then
+    (
 
-  // Your Code Goes Here
+      function(response)
+      {
+        if(response.status !== 200)
+        {
+          console.log('looks like there was a problem. Its a trap!' +response.status);
+          return;
+        }
+
+
+
+      response.json().then(function(data){
+        console.log(data.results[4].name.first);
+        console.log(data.results.length);
+
+
+        let userArray = [];
+
+        for (var i = 0; i < data.results.length; i++) {
+            let users = {}
+            users.img = data.results[i].picture.medium;
+            users.name = data.results[i].name.first + " " +  data.results[i].name.last;
+            users.company = " ";
+            users.address =  data.results[i].location.street + " " + data.results[i].location.city;
+            users.stateZip =  data.results[i].location.state + ", " +  data.results[i].location.postcode;
+            users.phone =  data.results[i].phone;
+
+            userArray.push(users);
+
+          }
+          console.log(userArray);
+
+
+          function renderUser(array)
+          {
+            let parentNode = document.getElementById('customers');
+            let ulNode = document.createElement('ul');
+            ulNode.id = "customer-container";
+
+            parentNode.appendChild(ulNode);
+
+            for (var i = 0; i < array.length; i++)
+            {
+              let listMaker= document.createElement('li');
+              let childNode = array[i];
+              ulNode.appendChild(childNode);
+            }
+
+          }
+
+
+          renderUser(userArray);
+
+        });
+      }
+    );
 
 })();
